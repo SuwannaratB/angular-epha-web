843c4d4d0cab6bc75c4892cf7e36cdea2123a608
kanompang962 <kanompang962@gmail.com>
2024-08-14 16:44:13 +0700

    setup tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { IndexComponent } from './layouts/index/index.component';
 const routes: Routes = [
   {path: '', component: IndexComponent,
     children: [
+      { path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
       { path: 'hazid', loadChildren: () => import('./modules/hazid/hazid.module').then(m => m.HazidModule) }, 
       { path: 'psa', loadChildren: () => import('./modules/psa/psa.module').then(m => m.PsaModule) },
       { path: 'lopa', loadChildren: () => import('./modules/lopa/lopa.module').then(m => m.LopaModule) }
     ]
-  }
+  },
 ];
 
 @NgModule({
